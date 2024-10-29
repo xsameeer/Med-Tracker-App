ee23d0f7f0f77dd38c67223f6984f50d1bcd9dea
xsameeer <dhanaliwalas@vcu.edu>
2024-10-29 15:01:11 -0400

    Added LoginActivity and User class for authentication model

---
+++ app/src/main/java/com/example/medtrackerapp/model/User.java
@@ -0,0 +1,27 @@
+package com.example.medtrackerapp.model;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User() {
+        this.username = null;
+        this.password = null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
