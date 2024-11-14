b269bf4077b88b30d2fc4fade8749b20f3c9d2bc
xDevinx <devinhale353@gmail.com>
2024-11-13 22:06:58 -0500

    Update 2.6

--- app/src/main/java/com/example/medtrackerapp/ProfileActivity.java
+++ app/src/main/java/com/example/medtrackerapp/ProfileActivity.java
@@ -1,4 +1,41 @@
 package com.example.medtrackerapp;
 
-public class ProfileActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.medtrackerapp.database.DatabaseHandler;
+
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView nameTxtView;
+    private TextView ageTxtView;
+    private TextView genderTxtView;
+    private TextView healthcareProviderTxtView;
+    private ImageView profileImageView;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        nameTxtView = findViewById(R.id.Name);
+        ageTxtView = findViewById(R.id.Age);
+        genderTxtView = findViewById(R.id.Gender);
+        healthcareProviderTxtView = findViewById(R.id.healthcare_provider);
+        profileImageView = findViewById(R.id.profilePicture);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("UserProfile", MODE_PRIVATE);
+        String name = sharedPreferences.getString("Name", "No name");
+        int age = sharedPreferences.getInt("Age", 0);
+        String gender = sharedPreferences.getString("Gender", "Unknown");
+        String healthcareProvider = sharedPreferences.getString("Healthcare Provider", "N/A");
+
+        nameTxtView.setText("Name: " + name);
+        ageTxtView.setText("Age: " + age);
+        genderTxtView.setText("Gender: " + gender);
+        healthcareProviderTxtView.setText("Healtcare Provider " + healthcareProvider);
+
+        //Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+    }
 }
