f014759e65e6e01abdc99426c8c2a60c474c6c21
xsameeer <dhanaliwalas@vcu.edu>
2024-11-13 19:08:24 -0500

    Update 2.0 {added medication, calendar, settings, profile class}

--- app/src/main/java/com/example/medtrackerapp/MedicationActivity.java
+++ app/src/main/java/com/example/medtrackerapp/MedicationActivity.java
@@ -1,4 +1,13 @@
 package com.example.medtrackerapp;
 
-public class MedicationActivity {
+import android.os.Bundle;
+import android.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MedicationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
 }
