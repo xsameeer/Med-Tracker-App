77526b6fbe85defed1a4876687268c4b98ddaed7
xDevinx <devinhale353@gmail.com>
2024-12-02 12:50:56 -0500

    Update 3.6 {Calendar page implemented}

--- app/src/main/java/com/example/medtrackerapp/CalendarActivity.java
+++ app/src/main/java/com/example/medtrackerapp/CalendarActivity.java
@@ -1,4 +1,32 @@
 package com.example.medtrackerapp;
 
-public class CalendarActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView textView;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        textView = (TextView) findViewById(R.id.date_view);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                String date = (month + 1) + "/" + day + "/" + year;
+                textView.setText(date);
+
+            }
+
+
+        });
+
+    }
 }
